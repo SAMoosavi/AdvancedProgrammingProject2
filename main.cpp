cd2be644e1e5ef1fbbd14ca3ff97d369a3808820
Moosavi <moosavi238@gmail.com>
2022-06-12 23:46:36 +0430

    create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "mainwindow.h"
+#include "user.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    User * myUser = new User;
+    QApplication a(argc, argv);
+    MainWindow w(myUser);
+    w.show();
+    return a.exec();
+}
