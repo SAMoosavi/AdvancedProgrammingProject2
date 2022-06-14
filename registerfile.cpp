d316640afa9b25b701cc5a2eeccbf2c83a5ae050
Moosavi <moosavi238@gmail.com>
2022-06-14 23:45:00 +0430

    show QMessageBox in registerFile

--- registerfile.cpp
+++ registerfile.cpp
@@ -1,14 +1,12 @@
 #include "registerfile.h"
 #include "ui_registerfile.h"
 
-RegisterFile::RegisterFile(User * myUser,QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::RegisterFile)
+RegisterFile::RegisterFile(User *myUser, QWidget *parent) : QMainWindow(parent),
+                                                            ui(new Ui::RegisterFile)
 {
     ui->setupUi(this);
-    this->myUser  = myUser;
+    this->myUser = myUser;
 }
-
 
 RegisterFile::~RegisterFile()
 {
@@ -17,33 +15,38 @@ RegisterFile::~RegisterFile()
 
 void RegisterFile::on_pushButton_clicked()
 {
-   QString confirmPassword = ui->lineEdit_confirm_password->text();
-   QString password = ui->lineEdit_password->text();
-   QString username = ui->lineEdit_username->text();
+    QString confirmPassword = ui->lineEdit_confirm_password->text();
+    QString password = ui->lineEdit_password->text();
+    QString username = ui->lineEdit_username->text();
 
-  switch(this->myUser->Register(username,password,confirmPassword)){
-      case registered:
-      qDebug() << "hi";
-      break;
-  case EVPassword:
-      qDebug() << "EVPassword";
-      break;
-  case EConfirmPassword:
-      qDebug() << "EConfirmPassword";
-      break;
-  case notAvailableUsername:
-      qDebug() << "notAvailableUsername";
-      break;
-  default:
-      qDebug() << "bay";
-  }
+    switch (this->myUser->Register(username, password, confirmPassword))
+    {
+    case registered:
+        QMessageBox::information(this, "Sign Up", "You are sign up");
+        this->goToSingnInPage();
+        break;
+    case EVPassword:
+        QMessageBox::critical(this, "Error", "Your password is not strong enough!");
+        break;
+    case EConfirmPassword:
+        QMessageBox::critical(this, "Error", "Your confirm password is not correct!");
+        break;
+    case notAvailableUsername:
+        QMessageBox::critical(this, "Error", "Your username is not available!");
+        break;
+    default:
+        QMessageBox::warning(this, "?", "?");
+    }
 }
-
 
 void RegisterFile::on_pushButton_2_clicked()
 {
-    hide();
-    this->mainWindow = new MainWindow(this->myUser,this);
-    this->mainWindow->show();
+    this->goToSingnInPage();
 }
 
+void RegisterFile::goToSingnInPage()
+{
+    hide();
+    this->mainWindow = new MainWindow(this->myUser, this);
+    this->mainWindow->show();
+}
