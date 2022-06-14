4565f6a31c6e4d6805b38d639e4a044b8e6f8ccc
Moosavi <moosavi238@gmail.com>
2022-06-14 23:50:20 +0430

    add todo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,9 +4,8 @@
 #include <QPixmap>
 #include <QMessageBox>
 
-MainWindow::MainWindow(User * myUser,QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(User *myUser, QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     this->myUser = myUser;
     ui->setupUi(this);
@@ -21,27 +20,26 @@ void MainWindow::on_pushButton_clicked()
 {
     QString username = ui->lineEdit_username->text();
     QString password = ui->lineEdit_password->text();
-   switch(this->myUser->login(username,password)){
-       case logined:
-       QMessageBox::information(this, "Welcome!", "Welcome!");
-       break;
-   case notFuond:
-       QMessageBox::critical(this, "Error!", "Username not found!");
-       break;
-   case notCorrectPassword:
-       QMessageBox::critical(this, "Error!", "Password is not correct!");
-       break;
-   default:
-       QMessageBox::information(this, "default", ":)");
-
-   }
+    switch (this->myUser->login(username, password))
+    {
+    case logined:
+        QMessageBox::information(this, "Welcome!", "Welcome!");
+        //   TODO:chenge page
+        break;
+    case notFuond:
+        QMessageBox::critical(this, "Error!", "Username not found!");
+        break;
+    case notCorrectPassword:
+        QMessageBox::critical(this, "Error!", "Password is not correct!");
+        break;
+    default:
+        QMessageBox::information(this, "default", ":)");
+    }
 }
-
 
 void MainWindow::on_pushButton_2_clicked()
 {
     hide();
-    this->registerFile = new RegisterFile(this->myUser,this);
+    this->registerFile = new RegisterFile(this->myUser, this);
     this->registerFile->show();
 }
-
