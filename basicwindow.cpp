96132ec6b9fd7a67149c672116a26dde441196bf
SaeedARV <mohammadsaeedarvenaghi@gmail.com>
2022-06-30 00:48:59 +0430

    edited

--- basicwindow.cpp
+++ basicwindow.cpp
@@ -23,16 +23,13 @@ BasicWindow::BasicWindow(User *myUser, QWidget *parent) :
     ui->tableWidget_buy->setItem(0, 0, new QTableWidgetItem("Symbol"));
     ui->tableWidget_buy->setItem(0, 1, new QTableWidgetItem("Full Name"));
     ui->tableWidget_buy->setItem(0, 2, new QTableWidgetItem("Price"));
-    ui->tableWidget_buy->setItem(0, 3, new QTableWidgetItem("Value"));
-    int i = 1;
+    ui->tableWidget_buy->setItem(0, 3, new QTableWidgetItem("Market Capacity"));
     for(auto st: Stock::allStocks){
         ui->tableWidget_buy->insertRow(ui->tableWidget_buy->rowCount());
-        ui->tableWidget_buy->setItem(i, 0, new QTableWidgetItem(st.second->symbol));
-        ui->tableWidget_buy->setItem(i, 1, new QTableWidgetItem(st.second->name));
-        ui->tableWidget_buy->setItem(i, 2, new QTableWidgetItem(QString::number(st.second->price)));
-        //TODO: save amount and use it
-        ui->tableWidget_buy->setItem(i, 3, new QTableWidgetItem(QString::number(st.second->price)));
-        i++;
+        ui->tableWidget_buy->setItem(st.first+1, 0, new QTableWidgetItem(st.second->symbol));
+        ui->tableWidget_buy->setItem(st.first+1, 1, new QTableWidgetItem(st.second->name));
+        ui->tableWidget_buy->setItem(st.first+1, 2, new QTableWidgetItem(QString::number(st.second->price)));
+        ui->tableWidget_buy->setItem(st.first+1, 3, new QTableWidgetItem(QString::number(st.second->marketCap)));
     }
 
 }
